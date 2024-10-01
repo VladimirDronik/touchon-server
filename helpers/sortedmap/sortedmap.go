17760f1b5f98da68d1be022ba1e7fe0d8f37158d
Oleg Shtykov <oleg.shtykov@gmail.com>
2024-10-01 16:42:25 +0300

    +sorted map

---
+++ helpers/sortedmap/sortedmap.go
@@ -0,0 +1,186 @@
+package sortedmap
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+type KeyType interface {
+	comparable
+}
+
+func New[K KeyType, V any]() *SortedMap[K, V] {
+	return &SortedMap[K, V]{
+		m: make(map[K]V, 0),
+	}
+}
+
+type Item[K KeyType, V any] struct {
+	Key   K
+	Value V
+}
+
+type SortedMap[K KeyType, V any] struct {
+	m     map[K]V
+	order []K
+}
+
+func (o *SortedMap[K, V]) Add(k K, v V) error {
+	if _, ok := o.m[k]; ok {
+		return errors.Wrap(errors.Errorf("value with key %v is exists", k), "Add")
+	}
+
+	o.m[k] = v
+	o.order = append(o.order, k)
+
+	return nil
+}
+
+func (o *SortedMap[K, V]) Set(k K, v V) {
+	if _, ok := o.m[k]; !ok {
+		_ = o.Add(k, v)
+		return
+	}
+
+	o.m[k] = v
+}
+
+func (o *SortedMap[K, V]) Get(k K) (V, error) {
+	var null V
+
+	v, ok := o.m[k]
+	if !ok {
+		return null, errors.Wrap(errors.Errorf("value with key %v not found", k), "Get")
+	}
+
+	return v, nil
+}
+
+func (o *SortedMap[K, V]) GetKeyValueList() []*Item[K, V] {
+	r := make([]*Item[K, V], 0, len(o.m))
+
+	for _, k := range o.order {
+		r = append(r, &Item[K, V]{k, o.m[k]})
+	}
+
+	return r
+}
+
+func (o *SortedMap[K, V]) GetValueList() []V {
+	r := make([]V, 0, len(o.m))
+
+	for _, k := range o.order {
+		r = append(r, o.m[k])
+	}
+
+	return r
+}
+
+func (o *SortedMap[K, V]) GetUnorderedMap() map[K]V {
+	r := make(map[K]V, len(o.m))
+
+	// copy map
+	for k, v := range o.m {
+		r[k] = v
+	}
+
+	return r
+}
+
+func (o *SortedMap[K, V]) Clear() {
+	for k := range o.m {
+		delete(o.m, k)
+	}
+	o.order = o.order[:0]
+}
+
+func (o *SortedMap[K, V]) MarshalJSON() ([]byte, error) {
+	keys := make([]json.RawMessage, 0, len(o.m))
+	values := make([]json.RawMessage, 0, len(o.m))
+
+	for _, k := range o.order {
+		key, err := json.Marshal(k)
+		if err != nil {
+			return nil, err
+		}
+
+		keys = append(keys, key)
+
+		value, err := json.Marshal(o.m[k])
+		if err != nil {
+			return nil, err
+		}
+
+		values = append(values, value)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	buf.WriteString("{")
+
+	for i, key := range keys {
+		if len(key) == 0 {
+			return nil, errors.Errorf("key is empty")
+		}
+
+		if key[0] != '"' {
+			buf.WriteByte('"')
+		}
+		buf.Write(keys[i])
+		if key[0] != '"' {
+			buf.WriteByte('"')
+		}
+		buf.WriteString(": ")
+		buf.Write(values[i])
+		if i+1 < len(keys) {
+			buf.WriteByte(',')
+		}
+	}
+
+	buf.WriteByte('}')
+
+	return buf.Bytes(), nil
+}
+
+func (o *SortedMap[K, V]) UnmarshalJSON(data []byte) error {
+	dec := json.NewDecoder(bytes.NewReader(data))
+
+	// read {
+	t, err := dec.Token()
+	if err != nil {
+		return err
+	}
+
+	for dec.More() {
+		// read key
+		t, err = dec.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		k, ok := t.(K)
+		if !ok {
+			return errors.Errorf("%v is not %T", t, k)
+		}
+
+		var v V
+		if err := dec.Decode(&v); err != nil {
+			return err
+		}
+
+		o.Set(k, v)
+	}
+
+	// read }
+	_, err = dec.Token()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ helpers/sortedmap/sortedmap_test.go
@@ -0,0 +1,106 @@
+package sortedmap
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func ExampleSortedMap_MarshalJSON() {
+	type Item struct {
+		Key   string
+		Value string
+	}
+
+	m1 := New[string, *Item]()
+	m1.Set("y", &Item{"y", "1"})
+	m1.Set("x", &Item{"x", "2"})
+
+	data, err := json.MarshalIndent(m1, "", "  ")
+	fmt.Println(string(data), err)
+
+	m2 := New[int, *Item]()
+	m2.Set(2, &Item{"y", "2"})
+	m2.Set(1, &Item{"x", "1"})
+
+	data, err = json.MarshalIndent(m2, "", "  ")
+	fmt.Println(string(data), err)
+
+	// Output: {
+	//   "y": {
+	//     "Key": "y",
+	//     "Value": "1"
+	//   },
+	//   "x": {
+	//     "Key": "x",
+	//     "Value": "2"
+	//   }
+	// } <nil>
+	// {
+	//   "2": {
+	//     "Key": "y",
+	//     "Value": "2"
+	//   },
+	//   "1": {
+	//     "Key": "x",
+	//     "Value": "1"
+	//   }
+	// } <nil>
+}
+
+func ExampleSortedMap_UnmarshalJSON() {
+	type Item struct {
+		Value int `json:"v"`
+	}
+
+	data := []byte(`{"a": {"v":1},"c": {"v":2},"b": {"v":3}}`)
+	m1 := New[string, Item]()
+	_ = json.Unmarshal(data, &m1)
+
+	data, _ = json.MarshalIndent(m1, "", "  ")
+	fmt.Println(string(data))
+
+	// ----
+
+	m2 := New[string, *Item]()
+	_ = json.Unmarshal(data, &m2)
+
+	data, _ = json.MarshalIndent(m2, "", "  ")
+	fmt.Println(string(data))
+
+	// ----
+
+	data = []byte(`{"a": 1,"c": 2,"b": 3}`)
+	m3 := New[string, int]()
+	_ = json.Unmarshal(data, &m3)
+
+	data, _ = json.MarshalIndent(m3, "", "  ")
+	fmt.Println(string(data))
+
+	// Output: {
+	//   "a": {
+	//     "v": 1
+	//   },
+	//   "c": {
+	//     "v": 2
+	//   },
+	//   "b": {
+	//     "v": 3
+	//   }
+	// }
+	// {
+	//   "a": {
+	//     "v": 1
+	//   },
+	//   "c": {
+	//     "v": 2
+	//   },
+	//   "b": {
+	//     "v": 3
+	//   }
+	// }
+	// {
+	//   "a": 1,
+	//   "c": 2,
+	//   "b": 3
+	// }
+}
