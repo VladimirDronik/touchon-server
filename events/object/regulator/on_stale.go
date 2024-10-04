b46773b823c46f54c6c8cf0c86dd7d8eee6d718f
Oleg Shtykov <oleg.shtykov@gmail.com>
2024-10-04 23:09:39 +0300

    fix events/object/regulator/NewOnStalerMessage()

--- events/object/regulator/on_stale.go
+++ events/object/regulator/on_stale.go
@@ -2,7 +2,6 @@ package regulator
 
 import (
 	"github.com/VladimirDronik/touchon-server/event"
-	"github.com/VladimirDronik/touchon-server/models"
 	"github.com/VladimirDronik/touchon-server/mqtt/messages"
 	"github.com/pkg/errors"
 )
@@ -17,19 +16,6 @@ func init() {
 			TargetType:  messages.TargetTypeObject,
 		}
 
-		v := &event.Prop{
-			Code: "value",
-			Name: "Значение",
-			Item: &models.Item{
-				Type:       models.DataTypeFloat,
-				RoundFloat: true,
-			},
-		}
-
-		if err := e.Props.Add(v); err != nil {
-			return nil, errors.Wrap(err, "init.maker")
-		}
-
 		return e, nil
 	}
 
@@ -38,8 +24,8 @@ func init() {
 	}
 }
 
-func NewOnStaleMessage(topic string, targetID int, value float32) (messages.Message, error) {
-	e, err := event.MakeEvent("object.regulator.on_stale", messages.TargetTypeObject, targetID, map[string]interface{}{"value": value})
+func NewOnStaleMessage(topic string, targetID int) (messages.Message, error) {
+	e, err := event.MakeEvent("object.regulator.on_stale", messages.TargetTypeObject, targetID, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "NewOnStaleMessage")
 	}
