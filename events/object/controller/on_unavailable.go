4e4d54275c182a671c2fb003a2c4951bba9e589e
VladimirDronik <kinord@yandex.ru>
2024-10-28 13:11:05 +0300

    Добавлены события для контроллера: on_load и on_unavailable

---
+++ events/object/controller/on_unavailable.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"github.com/VladimirDronik/touchon-server/event"
+	"github.com/VladimirDronik/touchon-server/mqtt/messages"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	maker := func() (*event.Event, error) {
+		e := &event.Event{
+			Code:        "object.controller.on_unavailable",
+			Name:        "Контроллер стал недоступен",
+			Description: "",
+			Props:       event.NewProps(),
+			TargetType:  messages.TargetTypeObject,
+		}
+
+		return e, nil
+	}
+
+	if err := event.Register(maker); err != nil {
+		panic(err)
+	}
+}
+
+func NewOnUnavailableMessage(topic string, targetID int) (messages.Message, error) {
+	e, err := event.MakeEvent("object.controller.on_unavailable", messages.TargetTypeObject, targetID, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnUnavailableMessage")
+	}
+
+	m, err := e.ToMqttMessage(topic)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnUnavailableMessage")
+	}
+
+	return m, nil
+}
