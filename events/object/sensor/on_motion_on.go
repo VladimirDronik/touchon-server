7f0cd25b891b1349685b601858854d09a0857183
Oleg Shtykov <oleg.shtykov@gmail.com>
2024-10-31 15:09:24 +0300

    +events: motion,presence

---
+++ events/object/sensor/on_motion_on.go
@@ -0,0 +1,40 @@
+package sensor
+
+import (
+	"github.com/VladimirDronik/touchon-server/event"
+	"github.com/VladimirDronik/touchon-server/mqtt/messages"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	maker := func() (*event.Event, error) {
+		e := &event.Event{
+			Code:        "object.sensor.on_motion_on",
+			Name:        "Движение есть",
+			Description: "",
+			Props:       event.NewProps(),
+			TargetType:  messages.TargetTypeObject,
+		}
+
+		return e, nil
+	}
+
+	// Для регистрации событий надо в service/init.go добавить импорт данного _пакета_!
+	if err := event.Register(maker); err != nil {
+		panic(err)
+	}
+}
+
+func NewOnMotionOnMessage(topic string, targetID int) (messages.Message, error) {
+	e, err := event.MakeEvent("object.sensor.on_motion_on", messages.TargetTypeObject, targetID, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnMotionOnMessage")
+	}
+
+	m, err := e.ToMqttMessage(topic)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnMotionOnMessage")
+	}
+
+	return m, nil
+}
---
+++ events/object/sensor/on_presence_on.go
@@ -0,0 +1,40 @@
+package sensor
+
+import (
+	"github.com/VladimirDronik/touchon-server/event"
+	"github.com/VladimirDronik/touchon-server/mqtt/messages"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	maker := func() (*event.Event, error) {
+		e := &event.Event{
+			Code:        "object.sensor.on_presence_on",
+			Name:        "Присутствие есть",
+			Description: "",
+			Props:       event.NewProps(),
+			TargetType:  messages.TargetTypeObject,
+		}
+
+		return e, nil
+	}
+
+	// Для регистрации событий надо в service/init.go добавить импорт данного _пакета_!
+	if err := event.Register(maker); err != nil {
+		panic(err)
+	}
+}
+
+func NewOnPresenceOnMessage(topic string, targetID int) (messages.Message, error) {
+	e, err := event.MakeEvent("object.sensor.on_presence_on", messages.TargetTypeObject, targetID, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnPresenceOnMessage")
+	}
+
+	m, err := e.ToMqttMessage(topic)
+	if err != nil {
+		return nil, errors.Wrap(err, "NewOnPresenceOnMessage")
+	}
+
+	return m, nil
+}
