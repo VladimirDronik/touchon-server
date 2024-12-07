2f0e9c9a93112136e55da1d945c61257def7fdd2
VladimirDronik <kinord@yandex.ru>
2024-12-07 19:58:20 +0300

    Добавлен метод для реле

--- service/init.go
+++ service/init.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/VladimirDronik/touchon-server/events/object/generic_input"
 	_ "github.com/VladimirDronik/touchon-server/events/object/port"
 	_ "github.com/VladimirDronik/touchon-server/events/object/regulator"
+	_ "github.com/VladimirDronik/touchon-server/events/object/relay"
 	_ "github.com/VladimirDronik/touchon-server/events/object/sensor"
 	_ "github.com/VladimirDronik/touchon-server/events/script"
 )
