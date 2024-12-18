0701160c18cfa69f3209c9618370cc683b4d0318
Oleg Shtykov <oleg.shtykov@gmail.com>
2024-12-18 08:11:03 +0300

    +events/object/modbus/wb_mrm2_mini/on_check

--- service/init.go
+++ service/init.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/VladimirDronik/touchon-server/events/item"
 	_ "github.com/VladimirDronik/touchon-server/events/object/controller"
 	_ "github.com/VladimirDronik/touchon-server/events/object/generic_input"
+	_ "github.com/VladimirDronik/touchon-server/events/object/modbus/wb_mrm2_mini"
 	_ "github.com/VladimirDronik/touchon-server/events/object/port"
 	_ "github.com/VladimirDronik/touchon-server/events/object/regulator"
 	_ "github.com/VladimirDronik/touchon-server/events/object/relay"
