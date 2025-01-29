6a5542af462d16997206766b7579e92cc32c330f
Oleg Shtykov <oleg.shtykov@gmail.com>
2025-01-29 17:51:54 +0300

    events/Onokom: -on_method_result, +on_change

--- events/object/onokom/gateway/on_check.go
+++ events/object/onokom/gateway/on_check.go
@@ -42,6 +42,30 @@ var VSlatsModes = map[string]string{
 	"6": "Пятое положение",
 }
 
+var props = []*event.Prop{
+	{Code: "power_status", Name: "Состояние вкл/выкл", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "display_backlight", Name: "Подсветка экрана", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "display_brightness", Name: "Яркость экрана", Item: &models.Item{Type: models.DataTypeInt}},
+	{Code: "silent_mode", Name: "Режим Тихий", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "eco_mode", Name: "Режим Эко", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "turbo_mode", Name: "Режим Турбо", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "sleep_mode", Name: "Режим Сон", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "ionization", Name: "Ионизация", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "self_cleaning", Name: "Самоочистка", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "anti_fungus", Name: "Антиплесень", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "disable_display_on_power_off", Name: "Отключение экрана при отключенном устройстве", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "sounds", Name: "Звуковая индикация", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "on_duty_heating", Name: "Дежурный обогрев", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "soft_flow", Name: "Мягкий поток", Item: &models.Item{Type: models.DataTypeBool}},
+	{Code: "operating_mode", Name: "Режим работы", Item: &models.Item{Type: models.DataTypeEnum, Values: OpModes}},
+	{Code: "internal_temperature", Name: "Температура в помещении", Item: &models.Item{Type: models.DataTypeInt}},
+	{Code: "external_temperature", Name: "Температура на улице", Item: &models.Item{Type: models.DataTypeInt}},
+	{Code: "target_temperature", Name: "Целевая температура", Item: &models.Item{Type: models.DataTypeInt}},
+	{Code: "fan_speed", Name: "Скорость вентилятора", Item: &models.Item{Type: models.DataTypeEnum, Values: FanSpeed}},
+	{Code: "horizontal_slats_mode", Name: "Режим работы горизонтальных ламелей", Item: &models.Item{Type: models.DataTypeEnum, Values: HSlatsModes}},
+	{Code: "vertical_slats_mode", Name: "Режим работы вертикальных ламелей", Item: &models.Item{Type: models.DataTypeEnum, Values: VSlatsModes}},
+}
+
 func init() {
 	maker := func() (*event.Event, error) {
 		e := &event.Event{
@@ -50,30 +74,6 @@ func init() {
 			Description: "Получение состояния устройства",
 			Props:       event.NewProps(),
 			TargetType:  messages.TargetTypeObject,
-		}
-
-		props := []*event.Prop{
-			{Code: "power_status", Name: "Состояние вкл/выкл", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "display_backlight", Name: "Подсветка экрана", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "display_brightness", Name: "Яркость экрана", Item: &models.Item{Type: models.DataTypeInt}},
-			{Code: "silent_mode", Name: "Режим Тихий", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "eco_mode", Name: "Режим Эко", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "turbo_mode", Name: "Режим Турбо", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "sleep_mode", Name: "Режим Сон", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "ionization", Name: "Ионизация", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "self_cleaning", Name: "Самоочистка", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "anti_fungus", Name: "Антиплесень", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "disable_display_on_power_off", Name: "Отключение экрана при отключенном устройстве", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "sounds", Name: "Звуковая индикация", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "on_duty_heating", Name: "Дежурный обогрев", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "soft_flow", Name: "Мягкий поток", Item: &models.Item{Type: models.DataTypeBool}},
-			{Code: "operating_mode", Name: "Режим работы", Item: &models.Item{Type: models.DataTypeEnum, Values: OpModes}},
-			{Code: "internal_temperature", Name: "Температура в помещении", Item: &models.Item{Type: models.DataTypeInt}},
-			{Code: "external_temperature", Name: "Температура на улице", Item: &models.Item{Type: models.DataTypeInt}},
-			{Code: "target_temperature", Name: "Целевая температура", Item: &models.Item{Type: models.DataTypeInt}},
-			{Code: "fan_speed", Name: "Скорость вентилятора", Item: &models.Item{Type: models.DataTypeEnum, Values: FanSpeed}},
-			{Code: "horizontal_slats_mode", Name: "Режим работы горизонтальных ламелей", Item: &models.Item{Type: models.DataTypeEnum, Values: HSlatsModes}},
-			{Code: "vertical_slats_mode", Name: "Режим работы вертикальных ламелей", Item: &models.Item{Type: models.DataTypeEnum, Values: VSlatsModes}},
 		}
 
 		if err := e.Props.Add(props...); err != nil {
